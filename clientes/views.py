6a6d433d2ebd611dd9de173246943dbab597d1be
71180leo-wq <https://github.com/71180leo-wq/mysiteciber.git>
2026-06-21 20:34:01 -0600

    conexion

--- clientes/views.py
+++ clientes/views.py
@@ -8,3 +8,16 @@ def listaclientes(request):
   #  return HttpResponse("FORMULARIO DE CIENTES JESUS ")
   return render(request, "clientes/clientes.html")
 
+def creaclientes(request):
+  nvocliente = clientes(
+  nombre = request.POST["nombres"],
+  apellido =  request.POST["apellido"],
+  sexo = request.POST["sexo"],
+  tipo = request.POST["tipo"],
+  direccion = request.POST["direccion"]
+  )
+
+nvocliente.save()
+return redirect("/pageclientes/")
+
+
