96091305a5a44c3125c46486e572386d21503d25
71180leo-wq <https://github.com/71180leo-wq/mysiteciber.git>
2026-06-21 18:35:04 -0600

    conexion

--- productos/models.py
+++ productos/models.py
@@ -4,7 +4,5 @@ from django.db import models
 
 class productos(models.Model):
     nombre = models.CharField()
-    apellido =  models.CharField()
-    sexo = models.CharField()
     tipo = models.CharField()
     direccion = models.CharField()
