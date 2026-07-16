db042084df8beeeac7b077e2ca015f48931f82a2
71180leo-wq <https://github.com/71180leo-wq/mysiteciber.git>
2026-07-15 18:09:47 -0600

    Examen segundo parcial- Jesus ramirez

--- mysiteramirez/settings.py
+++ mysiteramirez/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'clientes',
     'nomina',
+    'grupos',
     'proveedores',
     'usuarios',
     'productos',
--- usuarios/models.py
+++ usuarios/models.py
@@ -8,3 +8,11 @@ class usuarios(models.Model):
     sexo = models.CharField()
     tipo = models.CharField()
     direccion = models.CharField()
+
+class grupos(models.Model):
+    nombre = models.CharField()
+    apellido =  models.CharField()
+    sexo = models.CharField()
+    tipo = models.CharField()
+    direccion = models.CharField()
+    usuarios = models.ForeignKey(usuarios, on_delete= models.CASCADE, related_name="grupos")
